b97523fd17aeed1b0880b8478408162f0c8ad28c
Johannes Egger <eggerjohansi@gmail.com>
2016-08-18 21:20:32 +0200

    Set default path to php.exe

--- build.bat
+++ build.bat
@@ -5,6 +5,7 @@ set uploadUrl=
 set uploadUsername=
 set uploadPassword=
 set tempUrl=
+set phpExePath=php.exe
 set activitiesPath=
 
 .\packages\FAKE\tools\FAKE.exe .\Scripts\Build\run.fsx %* ^
@@ -14,7 +15,8 @@ set activitiesPath=
 -ev upload-url %uploadUrl% ^
 -ev temp-url %tempUrl% ^
 -ev upload-username %uploadUsername% ^
--ev upload-password %uploadPassword%
+-ev upload-password %uploadPassword% ^
+-ev php-exe-path %phpExePath%
 
 REM pushd .\scripts\build
 
