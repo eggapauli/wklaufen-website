1dfa8a106da7f196bee8b5e6883c8aaccd4e526b
Johannes Egger <eggerjohansi@gmail.com>
2016-11-19 08:11:50 +0100

    Add log statements and redirect messages from child process

--- init.fsx
+++ init.fsx
@@ -41,16 +41,24 @@ let installPaketDependencies =
     InstallProcess.Install(options, false, dependenciesFile, lockFile)
 
 let downloadComposer =
+    printfn "Downloading composer if necessary"
     let setupFileName = Path.Combine(__SOURCE_DIRECTORY__, "composer-setup.php")
     let mainFileName = Path.Combine(__SOURCE_DIRECTORY__, "composer.phar")
     try
-        if not <| File.Exists mainFileName then
-            if not <| File.Exists setupFileName then
+        if File.Exists mainFileName
+        then printfn "%s already exists" mainFileName
+        else
+            printfn "%s doesn't exist" mainFileName
+            if File.Exists setupFileName
+            then printfn "%s already exists" setupFileName
+            else
+                printfn "%s doesn't exist" setupFileName
                 use client = new HttpClient()
                 let installerUrl = "https://getcomposer.org/installer"
                 use sourceStream = client.GetStreamAsync (Uri installerUrl) |> Async.AwaitTask |> Async.RunSynchronously
                 use targetStream = File.OpenWrite setupFileName
                 sourceStream.CopyToAsync targetStream |> Async.AwaitTask |> Async.RunSynchronously
+                printfn "Downloaded %s" setupFileName
             let expectedHash =
                 use client = new HttpClient()
                 let sigUrl = "https://composer.github.io/installer.sig"
@@ -65,8 +73,22 @@ let downloadComposer =
                 |> String.concat ""
             if actualHash = expectedHash
             then
-                use p = System.Diagnostics.Process.Start(pathToPhpExe, "composer-setup.php")
+                printfn "Hash is ok. Executing %s" setupFileName
+                let startInfo = System.Diagnostics.ProcessStartInfo(pathToPhpExe, setupFileName)
+                startInfo.RedirectStandardOutput <- true
+                startInfo.RedirectStandardError <- true
+                startInfo.UseShellExecute <- false
+                use p = System.Diagnostics.Process.Start startInfo
+                use d0 = p.OutputDataReceived.Subscribe(fun p -> printfn "%s" p.Data)
+                use d1 = p.ErrorDataReceived.Subscribe(fun p -> eprintfn "%s" p.Data)
+                p.BeginOutputReadLine()
+                p.BeginErrorReadLine()
+                
                 p.WaitForExit()
+                
+                if p.ExitCode = 0
+                then printfn "Successfully executed %s" setupFileName
+                else failwithf "Error while executing %s. Exit code was %d" setupFileName p.ExitCode
             else
                 actualHash |> printfn "Actual hash:   <%s>"
                 expectedHash |> printfn "Expected hash: <%s>"
