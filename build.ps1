c08a3e93e207ce01086e025b1607d50da53dcfd3
Johannes Egger <j.egger@posteo.at>
2021-07-27 08:18:54 +0200

    Fix local deploy scripts

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 dotnet fable .\src\WkLaufen.Website\ --run yarn --cwd .\src\WkLaufen.Website build
 dotnet run --project .\src\WkLaufen.Website.ServerGenerator
 
-Copy-Item .\src\WkLaufen.Website.Server\*.php .\public
-Copy-Item -Recurse .\src\WkLaufen.Website.Server\vendor .\public
+Copy-Item .\src\WkLaufen.Website.Server\*.php .\public -Force
+Copy-Item .\src\WkLaufen.Website.Server\vendor .\public -Recurse -Force
 7z a app.zip .\public\.
--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 $client = New-Object System.Net.WebClient
 $client.Credentials = New-Object System.Net.NetworkCredential($env:FTP_USER, $env:FTP_PASSWORD)
-$client.UploadFile("ftp://$env:FTP_HOST/tmp/deploy/app.zip", "app.zip")
-$client.UploadFile("ftp://$env:FTP_HOST/web/publish.php", "src\publish.php")
+$client.UploadFile("ftp://$env:FTP_HOST/tmp/deploy/app.zip", "$pwd\app.zip")
+$client.UploadFile("ftp://$env:FTP_HOST/web/publish.php", "$pwd\src\publish.php")
 Invoke-WebRequest -Uri "http://wk-laufen.at/publish.php?package-name=app.zip" | Out-Null
